aca58ecf72f159ad3593d7c06db063cac1fedc82
Tim Schindler <tim.schindler@gmail.com>
2022-01-07 22:50:04 +0100

    add MVP for cmdlet that creates a package for connection components

---
+++ New-PASConnectionComponentPackage.Tests.ps1
@@ -0,0 +1,52 @@
+﻿BeforeAll {
+    . .\New-PASConnectionComponentPackage.ps1
+}
+
+Describe 'New-PASConnectionComponentPackage' {
+    Context 'creating package.json' {
+        BeforeAll {
+            Out-File -Path "$TestDrive\Dispatcher.exe"
+            New-PASConnectionComponentPackage -ConnectionComponentId PSM-SampleApp `
+                -PackageFilesPath "$TestDrive" `
+                -ConnectionComponentApplicationPaths @('C:\SampleApp\SampleApp.exe', 'C:\SampleApp\Driver.exe') `
+                -DestinationPath "$TestDrive" `
+
+            Expand-Archive "$TestDrive\PSM-SampleApp.zip" -DestinationPath "$TestDrive\PSM-SampleApp"
+        }
+        It 'adds all connection component application paths to the clientapppaths array in package.json' {
+            $PackageJson = Get-Content -Path "$TestDrive\PSM-SampleApp\package.json" | ConvertFrom-Json
+            $PackageJson.ClientAppPaths | Should -HaveCount 2
+        }
+
+        It 'ensures that package.json is in the root of the zip archive' {
+            $ExpectedExpandedPackageJsonPath = "$TestDrive\PSM-SampleApp\package.json"
+            Get-Item -Path $ExpectedExpandedPackageJsonPath | Should -Exist
+        }
+    }
+
+    Context 'creating connection component xml settings' {
+        BeforeAll {
+            Out-File -Path "$TestDrive\Dispatcher.exe"
+            Copy-Item *.xml -Destination "$TestDrive\PVConfiguration.xml" -Force
+
+            New-PASConnectionComponentPackage -ConnectionComponentId PSM-RealVNC `
+                -PackageFilesPath "$TestDrive" `
+                -ConnectionComponentApplicationPaths @('C:\SampleApp\SampleApp.exe', 'C:\SampleApp\Driver.exe') `
+                -DestinationPath "$TestDrive" `
+                -CreateConnectionComponentXmlFile $true `
+                -PVConfigurationPath "$TestDrive\PVConfiguration.xml"
+
+            Expand-Archive "$TestDrive\PSM-RealVNC.zip" -DestinationPath "$TestDrive\PSM-RealVNC"
+        }
+
+        It 'extracts existing connection component settings based on the provided connection component id and creates it as a file' {
+            $ExpectedConnectionComponentXmlFile = "$TestDrive\PSM-RealVNC\PSM-RealVNC.xml"
+            Get-Item -Path $ExpectedConnectionComponentXmlFile | Should -Exist
+        }
+
+        It 'ensures that the xml file is in the root of the zip archive' {
+            $ExpectedExpandedPackageJsonPath = "$TestDrive\PSM-RealVNC\PSM-RealVNC.xml"
+            Get-Item -Path $ExpectedExpandedPackageJsonPath | Should -Exist
+        }
+    }
+}
---
+++ New-PASConnectionComponentPackage.ps1
@@ -0,0 +1,94 @@
+﻿function New-PASConnectionComponentPackage {
+    param (
+        # The unique ID of the connection component. This will be used to name the zip archive and used to find an extract the connection component settings from an existing PVConfiguration.xml.
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $ConnectionComponentId,
+
+        # A path to a folder containing all the files to be a part of the connection component package.
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        # [ValidateScript( { Test-Path -Path $_ -PathType Leaf })]
+        [string]
+        $PackageFilesPath,
+
+        # An array of paths used by the connection component application. These paths will be used to update AppLocker rules.
+        [Parameter(Mandatory = $false)]
+        [string[]]
+        $ConnectionComponentApplicationPaths,
+
+        # Determines whether to create an XML file with the connection component settings from an existing PVConfiguration.xml
+        [Parameter(
+            ParameterSetName = 'CreateConnectionComponentXml',
+            Mandatory = $false
+            )]
+        [boolean]
+        $CreateConnectionComponentXmlFile = $false,
+
+        # File path of the PVConfiguration.xml to extract the connection component settings from.
+        [Parameter(
+            ParameterSetName = 'CreateConnectionComponentXml',
+            Mandatory = $false
+            )]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript( { Test-Path -Path $_ -PathType Leaf })]
+        [string]
+        $PVConfigurationPath,
+
+        # Folder path where to create the package zip archive.
+        [Parameter(Mandatory = $false)]
+        $DestinationPath = $PWD
+    )
+
+    begin {
+        $TemporaryDirectory = New-TemporaryDirectory
+    }
+    process {
+        # We create a new temporary directory as we need to save generated files somewhere in order to add it to our archive
+        $ConnectionComponentWorkingDirectory = "$TemporaryDirectory\$ConnectionComponentId"
+        New-Item -Path $ConnectionComponentWorkingDirectory -ItemType Directory
+
+        $FilesToArchive = @()
+
+        # Get the full file paths for all the files in $PackageFilesPath and add them to the array.
+        Get-ChildItem -Path $PackageFilesPath | ForEach-Object {$FilesToArchive += $_.FullName}
+
+        if ($ConnectionComponentApplicationPaths.Count -gt 0) {
+            $PackageJsonContent = [PSCustomObject]@{
+                PackageName = $ConnectionComponentId;
+                ClientAppPaths = @()
+            }
+
+            foreach ($Path in $ConnectionComponentApplicationPaths) {
+                $PackageJsonContent.ClientAppPaths += @{'Path' = $Path}
+            }
+
+            $PackageJsonContent | ConvertTo-Json | Out-File -FilePath "$ConnectionComponentWorkingDirectory\package.json" -Force
+            $FilesToArchive += "$ConnectionComponentWorkingDirectory\package.json"
+        }
+
+        if ($CreateConnectionComponentXmlFile) {
+            [xml]$PVConfigurationXml = Get-Content $PVConfigurationPath
+            $ConnectionComponentXml = Select-Xml -Xml $PVConfigurationXml -XPath "//ConnectionComponent[@Id='$ConnectionComponentId']"
+
+            if ($null -ne $ConnectionComponentXml) {
+                $Output = $ConnectionComponentXml.Node.OuterXml
+                $Output | Out-File -FilePath "$ConnectionComponentWorkingDirectory\$ConnectionComponentId.xml" -Force
+                $FilesToArchive += "$ConnectionComponentWorkingDirectory\$ConnectionComponentId.xml"
+            }
+        }
+
+        Compress-Archive -Path $FilesToArchive -DestinationPath "$DestinationPath\$ConnectionComponentId.zip"
+    }
+    end {
+        Remove-Item $TemporaryDirectory -Force -Recurse
+    }
+}
+
+# https://stackoverflow.com/a/34559554
+function New-TemporaryDirectory {
+    $parent = [System.IO.Path]::GetTempPath()
+    [string] $name = [System.Guid]::NewGuid()
+    New-Item -ItemType Directory -Path (Join-Path $parent $name)
+}
