b19be34c5590b6571ab1d5459a4e79dda121b49d
Tim Schindler <tim.schindler@gmail.com>
2022-01-13 19:12:16 +0100

    new-pasplatformpackage

---
+++ New-PASPlatformPackage.Tests.ps1
@@ -0,0 +1,28 @@
+﻿BeforeAll {
+    . .\New-PASPlatformPackage.ps1
+}
+
+Describe 'New-PASPlatformPackage' {
+    Context 'when creating the package' {
+        It 'accepts the plug-in files from the pipeline' {}
+        It 'it must contain a CPM policy file' {}
+        It 'it must contain a PVWA settings file' {}
+    }
+
+    Context 'when adding the CPM policy file' {
+        It 'names the .ini based on the platformid' {}
+        It 'adds an .ini file specified at a local path' {}
+        It 'retrieves an .ini from the Vault' {}
+    }
+
+    Context 'when creating the PVWA settings file' {
+        It 'names the PVWA settings file based on the platformid' {}
+        It 'extracts the PVWA settings out of an existing Policies.xml' {}
+        It 'can retrieve the Policies.xml from the Vault' {}
+    }
+
+    Context 'when creating a PVWA settings file for a platform' {
+        It 'extracts the appropriate device name' {}
+        It 'puts the PVWA settings as a child node to a device node' {}
+    }
+}
---
+++ New-PASPlatformPackage.ps1
@@ -0,0 +1,32 @@
+﻿function New-PASPlatformPackage {
+    param (
+        # Specifies the Id of the platform the package is being created for.
+        [Parameter(Mandatory = $true)]
+        [string]
+        $PlatformId,
+
+        # Specifies a path to the CPM Policy file to be included in the package.
+        [Parameter(Mandatory=$false)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $CPMPolicyFile,
+
+
+
+        # Specifies a path to one or more locations.
+        [Parameter(Mandatory=$true,
+                   Position=0,
+                   ParameterSetName="ParameterSetName",
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   HelpMessage="Path to one or more locations.")]
+        [Alias("PSPath")]
+        [ValidateNotNullOrEmpty()]
+        [string[]]
+        $Path
+    )
+
+    begin {}
+    process {}
+    end {}
+}
