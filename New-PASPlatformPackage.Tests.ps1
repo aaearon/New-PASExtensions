89da646999c5c8772e2226946e4a178120832bf6
Tim Schindler <tim.schindler@gmail.com>
2022-01-15 17:35:03 +0100

    a first version that is somewhat working

--- New-PASPlatformPackage.Tests.ps1
+++ New-PASPlatformPackage.Tests.ps1
@@ -1,12 +1,42 @@
 ﻿BeforeAll {
     . .\New-PASPlatformPackage.ps1
+
+
 }
 
 Describe 'New-PASPlatformPackage' {
+    BeforeAll {
+        $PlatformId = 'SamplePlatform'
+        $CreatedArchivePath = Join-Path -Path $TestDrive -ChildPath "$PlatformId.zip"
+        $ExpandedArchivePath = Join-Path -Path $TestDrive -ChildPath $PlatformId
+
+        Out-File -Path (Join-Path -Path $TestDrive -ChildPath "my-platforms-cpm-settings.ini") -Force
+        Out-File -Path (Join-Path -Path $TestDrive -ChildPath "my-platforms-pvwa-settings.xml") -Force
+
+        $BuildDirectory = Join-Path -Path $TestDrive -ChildPath 'Build'
+        New-Item -Path $BuildDirectory -ItemType Directory
+        Out-File -Path (Join-Path -Path $BuildDirectory -ChildPath 'Prompts.ini') -Force
+        Out-File -Path (Join-Path -Path $BuildDirectory -ChildPath 'Processes.ini') -Force
+
+        Get-ChildItem -Path $BuildDirectory `
+        | New-PASPlatformPackage -PlatformId $PlatformId `
+        -CPMPolicyFile (Join-Path -Path $TestDrive -ChildPath "my-platforms-cpm-settings.ini") `
+        -PVWASettingsFile (Join-Path -Path $TestDrive -ChildPath "my-platforms-pvwa-settings.xml") `
+        -DestinationPath "$TestDrive"
+
+        Expand-Archive $CreatedArchivePath -DestinationPath $ExpandedArchivePath
+    }
     Context 'when creating the package' {
-        It 'accepts the plug-in files from the pipeline' {}
-        It 'it must contain a CPM policy file' {}
-        It 'it must contain a PVWA settings file' {}
+        It 'accepts the plug-in files from the pipeline' {
+            Test-Path -Path "$TestDrive\$PlatformId\Prompts.ini" | Should -Be $true
+            Test-Path -Path "$TestDrive\$PlatformId\Processes.ini" | Should -Be $true
+        }
+        It 'it must contain a <Type> file named <Name>' {
+            Test-Path -Path (Join-Path -Path $TestDrive -ChildPath $PlatformId -AdditionalChildPath $Name) | Should -Be $true
+        }  -ForEach @(
+            @{Type = 'CPM policy'; Name = "Policy-SamplePlatform.ini"}
+            @{Type = 'PVWA settings'; Name = "Policy-SamplePlatform.xml"}
+        )
     }
 
     Context 'when adding the CPM policy file' {
@@ -24,5 +54,26 @@ Describe 'New-PASPlatformPackage' {
     Context 'when creating a PVWA settings file for a platform' {
         It 'extracts the appropriate device name' {}
         It 'puts the PVWA settings as a child node to a device node' {}
+    }
+}
+
+Describe 'Get-PlatformPVWASettings' {
+    It 'extracts the PVWA settings for a <PlatformType> out of an existing Policies.xml' {
+        $PlatformSettings = Get-PlatformPVWASettings -PoliciesFile '.\Policies.xml' -PlatformId $PlatformId
+        $PlatformSettingsXml = [xml]$PlatformSettings
+
+        Select-Xml -Xml $PlatformSettingsXml -XPath "//$PlatformType[@ID='$PlatformId']" | Should -Be $true
+    } -ForEach @(
+        @{PlatformType = 'Usage'; PlatformId = 'INIFile'}
+        @{PlatformType = 'Policy'; PlatformId = 'CyberArk'}
+    )
+
+    Context 'when getting the PVWA settings for a Policy' {
+        It 'puts the PVWA settings as a child node to a policies node which is a child to a device node' {
+            $PlatformSettings = Get-PlatformPVWASettings -PoliciesFile '.\Policies.xml' -PlatformId CyberArk
+            $PlatformSettingsXml = [xml]$PlatformSettings
+
+            Select-Xml -Xml $PlatformSettingsXml -XPath "//Device[@Name='Application']/Policies/Policy[@ID='CyberArk']" | Should -Be $true
+        }
     }
 }
--- New-PASPlatformPackage.ps1
+++ New-PASPlatformPackage.ps1
@@ -1,4 +1,5 @@
 ﻿function New-PASPlatformPackage {
+    [CmdletBinding()]
     param (
         # Specifies the Id of the platform the package is being created for.
         [Parameter(Mandatory = $true)]
@@ -6,27 +7,110 @@
         $PlatformId,
 
         # Specifies a path to the CPM Policy file to be included in the package.
-        [Parameter(Mandatory=$false)]
+        [Parameter(Mandatory = $true)]
+        [ValidateScript( { Test-Path -Path $_ -PathType Leaf })]
         [ValidateNotNullOrEmpty()]
         [string]
         $CPMPolicyFile,
 
+        [Parameter(Mandatory = $true)]
+        [ValidateScript( { Test-Path -Path $_ -PathType Leaf })]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $PVWASettingsFile,
 
+        [Parameter(Mandatory = $false)]
+        [ValidateScript( { Test-Path -Path $_ -PathType Leaf })]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $PoliciesFile,
 
         # Specifies a path to one or more locations.
-        [Parameter(Mandatory=$true,
-                   Position=0,
-                   ParameterSetName="ParameterSetName",
-                   ValueFromPipeline=$true,
-                   ValueFromPipelineByPropertyName=$true,
-                   HelpMessage="Path to one or more locations.")]
-        [Alias("PSPath")]
+        [Parameter(
+            ValueFromPipeline = $true,
+            Mandatory = $false
+        )]
         [ValidateNotNullOrEmpty()]
+        [ValidateScript( { Test-Path -Path $_ })]
         [string[]]
-        $Path
+        [Alias('PSPath')]
+        $Path,
+
+        # Folder path where to create the package zip archive.
+        [Parameter(Mandatory = $false)]
+        $DestinationPath = $PWD
     )
 
-    begin {}
-    process {}
-    end {}
+    begin {
+        $FilesToArchive = @()
+
+        $TemporaryDirectory = New-TemporaryDirectory
+        $PlatformWorkingDirectory = Join-Path -Path $TemporaryDirectory -ChildPath $PlatformId
+        New-Item -Path $PlatformWorkingDirectory -ItemType Directory
+
+        $CPMPolicyFile = Copy-Item -Path $CPMPolicyFile -Destination (Join-Path -Path $PlatformWorkingDirectory -ChildPath "Policy-$PlatformId.ini") -PassThru
+        $FilesToArchive += $CPMPolicyFile
+
+        $PVWASettingsFile = Copy-Item -Path $PVWASettingsFile -Destination (Join-Path -Path $PlatformWorkingDirectory -ChildPath "Policy-$PlatformId.xml") -PassThru
+        $FilesToArchive += $PVWASettingsFile
+    }
+    process {
+        if ($Path.Count -gt 0) {
+            $FilesToArchive += Get-ChildItem -Path $Path | ForEach-Object FullName
+        }
+        else {
+            Write-Debug "No platform files passed!"
+        }
+    }
+    end {
+        Compress-Archive -Path $FilesToArchive -DestinationPath (Join-Path -Path $DestinationPath -ChildPath "$PlatformId.zip") -CompressionLevel Fastest
+        Remove-Item $TemporaryDirectory -Force -Recurse
+    }
+}
+
+function Get-PlatformPVWASettings {
+    param (
+        $PlatformId,
+        $PoliciesFile
+    )
+
+    $FileXml = [xml](Get-Content -Path $PoliciesFile)
+    $SelectXPath = "//Device/Policies/Policy[@ID='$PlatformId'] | //Usage[@ID='$PlatformId']"
+    $PlatformXml = $FileXml.SelectSingleNode($SelectXPath)
+
+    if ($PlatformXml.Name -eq 'Usage') {
+        $SettingsXml = $PlatformXml
+
+    } elseif ($PlatformXml.Name -eq 'Policy') {
+
+        # Get name of the Device
+        $DeviceName = $PlatformXml.ParentNode.ParentNode.Name
+
+        # Create a new XML document.
+        $NewXml = New-Object xml
+
+        # Create the Device element with the Name attribute and add it to our new XML document.
+        $DeviceElement = $NewXml.CreateElement('Device')
+        $DeviceElement.SetAttribute('Name', $DeviceName)
+        # Must have Out-Null here as AppendChild returns an XmlNode and uncaptured output in PowerShell
+        # is implicity emitted to the pipeline.
+        $NewXml.AppendChild($DeviceElement) | Out-Null
+
+        $PoliciesElement = $NewXml.CreateElement('Policies')
+        $NewXml.Device.AppendChild($PoliciesElement) | Out-Null
+
+        $Policies = $NewXml.Device.SelectSingleNode('//Policies')
+        $Policy = $NewXml.ImportNode($PlatformXml, $true)
+        $Policies.AppendChild($Policy) | Out-Null
+
+        $SettingsXml = $NewXml
+
+    }
+        return $SettingsXml.OuterXml
+}
+
+function New-TemporaryDirectory {
+    $parent = [System.IO.Path]::GetTempPath()
+    [string] $name = [System.Guid]::NewGuid()
+    New-Item -ItemType Directory -Path (Join-Path $parent $name)
 }
